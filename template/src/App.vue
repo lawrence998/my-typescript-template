f0a9ade9d7cf8a1a766da8135d411a9120a5ffad
lichihang <lichihang@banggood.com>
2019-12-27 16:42:16 +0800

    新增通用样式

--- template/src/App.vue
+++ template/src/App.vue
@@ -31,12 +31,25 @@ export default class App extends Vue {}
 </script>
 
 <style>
+*{
+  padding: 0;
+  margin: 0;
+}
+ul{
+  list-style: none;
+}
+html {
+  height: 100%;
+}
+body {
+  height: 100%;
+  font-size: 14px;
+  color: #333;
+}
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  height: 100%;
 }
 </style>
