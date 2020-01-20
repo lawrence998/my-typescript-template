8332fee437353208e521c5ac1efd3169fe4922f6
lichihang <lichihang@banggood.com>
2020-01-20 14:20:31 +0800

    更新element-ui及eslint的版本

--- template/.eslintrc.js
+++ template/.eslintrc.js
@@ -4,14 +4,20 @@ module.exports = {
   root: true,
   parser: 'babel-eslint',
   parserOptions: {
+    parser: '@typescript-eslint/parser',
+    env: { es6: true },
     sourceType: 'module'
   },
   env: {
     browser: true,
+    node: true,
+    serviceworker: true
   },
   {{#if_eq lintConfig "standard"}}
+  plugins: ['vue', '@typescript-eslint'],
   // https://github.com/standard/standard/blob/master/docs/RULES-en.md
-  extends: 'standard',
+  // extends: 'standard',
+  extends: ['plugin:vue/base', 'plugin:@typescript-eslint/recommended', 'plugin:vue/essential', 'standard'],
   {{/if_eq}}
   {{#if_eq lintConfig "airbnb"}}
   extends: 'airbnb-base',
@@ -34,7 +40,31 @@ module.exports = {
   rules: {
     {{#if_eq lintConfig "standard"}}
     // allow async-await
-    'generator-star-spacing': 'off',
+    // 'generator-star-spacing': 'off',
+    // 设置默认eslint规则
+    'one-var': 0,
+    'arrow-parens': 0,
+    'generator-star-spacing': 0,
+    // 'no-debugger': 0,
+    'no-console': 0,
+    semi: [2, 'always'],
+    'no-extra-semi': 2,
+    'space-before-function-paren': 0,
+    eqeqeq: 0,
+    'spaced-comment': 0,
+    'no-useless-escape': 0,
+    'no-tabs': 0,
+    'no-mixed-spaces-and-tabs': 0,
+    'new-cap': 0,
+    camelcase: 0,
+    'no-new': 0,
+    indent: 'off',
+    semi: 'off',
+    // 设置typescript-eslint规则
+    // https://github.com/typescript-eslint/typescript-eslint/tree/master/packages/eslint-plugin/docs/rules
+    '@typescript-eslint/semi': ['error'],
+    '@typescript-eslint/indent': ['error', 2],
+    '@typescript-eslint/explicit-function-return-type': 0
     {{/if_eq}}
     {{#if_eq lintConfig "airbnb"}}
     // don't require .vue extension when importing
